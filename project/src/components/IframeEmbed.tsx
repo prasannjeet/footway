4d68b1e63f7bb50f023b56a05480528e1da8c8b3
Prasannjeet Singh <1756270+prasannjeet@users.noreply.github.com>
2024-12-14 11:44:09 +0000

    update build frontend, and use https

--- project/src/components/IframeEmbed.tsx
+++ project/src/components/IframeEmbed.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const IframeEmbed: React.FC = () => {
     return (
         <iframe
-            src="http://dify.homelab.ddnsguru.com/chatbot/mmDbumgz3CriXYXI"
+            src="https://dify.homelab.ddnsguru.com/chatbot/mmDbumgz3CriXYXI"
             style={{ width: '100%', height: '100%', minHeight: '700px' }}
             frameBorder="0"
             allow="microphone"
